d066eb453ea78ea64e3c1403a9f064175e431cf7
Maicon Costa <maiconcostawd@hotmail.com>
2020-05-01 15:45:49 -0300

    Registration of multiple items in the order

--- src/pages/NewOrder/index.js
+++ src/pages/NewOrder/index.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { FaMinus, FaPlus } from 'react-icons/fa';
 
 import {
     Button,
-    FormControl,
-    Input,
+    InputAdornment,
     InputLabel,
-    MenuItem,
-    Select,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
     TextField
 } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
@@ -19,11 +21,14 @@ import api from '../../services/api';
 
 export default function NewItem() {
     const [locale, setLocale] = useState('');
+    const [observation, setObservation] = useState('');
     const [total, setTotal] = useState(0);
     const statusId = 1;
 
     const [items, setItems] = useState([]);
     const [selectedItems, setSelectedItems] = useState([]);
+    const [countedSelectedItems, setCountedSelectedItems] = useState([]);
+    
     const history = useHistory();
 
     useEffect(() => {
@@ -34,7 +39,9 @@ export default function NewItem() {
         api.get('items', {
         }).then(response => {
             setItems(response.data);
-        })
+        }).catch((err) => {
+            console.log(err);
+        });
     }
 
     async function handleNewOrder(e) {
@@ -42,21 +49,22 @@ export default function NewItem() {
 
         const data = {
             locale,
+            observation,
             total,
             statusId,
-            items: selectedItems
+            items: countedSelectedItems
         };
 
-        try {
-            await api.post('order', data, {});
-            handleSuccess();
-            history.push('/pedidos');
-        } catch (err) {
-            handleError();
-        }
+        await api.post('order', data, {})
+            .then(() => {
+                toastSuccess();
+                history.push('/pedidos');
+            }).catch(() => {
+                toastError();
+            });
     }
 
-    function handleSuccess() {
+    function toastSuccess() {
         toast.success('Pedido cadastrado com sucesso!', {
             position: "bottom-center",
             autoClose: 5000,
@@ -67,7 +75,7 @@ export default function NewItem() {
         });
     }
 
-    function handleError() {
+    function toastError() {
         toast.error('Erro ao cadastrar o pedido, tente novamente.', {
             position: "bottom-center",
             autoClose: 5000,
@@ -77,6 +85,37 @@ export default function NewItem() {
             draggable: true
         });
     }
+
+    function handleSelectItem(item) {
+        setSelectedItems([...selectedItems, item]);
+        setTotal(total + item.value);
+    }
+
+    function handleRemoveSelectItem(item) {    
+        const leftoverItems = selectedItems
+            .filter(selectedItem => selectedItem.id !== item.id);
+
+        setSelectedItems(leftoverItems);
+        setTotal(total - item.value);
+    }
+
+
+    useEffect(() => {
+        const filterSelectedItems = selectedItems
+            .map(filterItem => filterItem)
+            .filter((item, index, array) => array.indexOf(item) === index);
+
+        const counts = filterSelectedItems
+            .map(filterItem => ({
+                id: filterItem.id,
+                count: selectedItems.filter(item => item.id === filterItem.id).length,
+                name: filterItem.name,
+                value: filterItem.value * selectedItems.filter(item => item.id === filterItem.id).length
+            }));
+
+        setCountedSelectedItems(counts);   
+    }, [selectedItems]);
+
 
     return (
         <div className="newOrderContainer">
@@ -92,37 +131,71 @@ export default function NewItem() {
                         margin="dense"
                         value={locale}
                         onChange={e => setLocale(e.target.value)}
+                        className="locale"
                         required
                     />
 
-                    <FormControl margin="dense">
-                        <InputLabel>Selecione os itens</InputLabel>
-                        <Select
-                            id="mutiple-name"
-                            label="Itens"
-                            multiple
-                            value={selectedItems}
-                            onChange={e => setSelectedItems(e.target.value)}
-                            input={<Input />}>
+                    <InputLabel margin="dense">Selecione os itens</InputLabel>
+
+                    <div className="items">
+                        <List component="nav" className="itemsList">
                             {items.map((item) => (
-                                <MenuItem key={item.id} value={item.id}>
-                                    {item.name}
-                                </MenuItem>
+                                <ListItem
+                                    key={item.id}
+                                    onClick={() => handleSelectItem(item)}
+                                    button>
+                                    <ListItemText 
+                                        primary={item.name}
+                                        secondary={item.description}
+                                     />
+                                    <ListItemIcon>
+                                        <FaPlus />
+                                    </ListItemIcon>
+                                </ListItem>
                             ))}
-                        </Select>
-                    </FormControl>
+                        </List>
+
+                        <List component="nav" className="selectedItemsList">
+                            {countedSelectedItems.map((selectedItem) => (
+                                <ListItem
+                                    key={selectedItem.id}
+                                    onClick={() => handleRemoveSelectItem(selectedItem)}
+                                    button>                                   
+                                    <ListItemText>{selectedItem.count} {selectedItem.name}</ListItemText>
+                                    <ListItemIcon>
+                                        <FaMinus />
+                                    </ListItemIcon>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </div>
+
+                    <TextField
+                        id="standard-basic"
+                        label="Observação"
+                        className="observation"
+                        multiline
+                        margin="dense"
+                        value={observation}
+                        onChange={e => setObservation(e.target.value)}
+                    />
 
                     <TextField
                         type="number"
-                        className="value"
                         id="standard-basic"
                         label="Total"
+                        className="value"
                         margin="dense"
                         value={total}
                         onChange={e => setTotal(e.target.value)}
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start">R$</InputAdornment>
+                        }}
                     />
 
-                    <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
+                    <div className="actions">
+                        <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
+                    </div>
                 </form>
             </div>
         </div>
